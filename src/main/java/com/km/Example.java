f530120d9e1e2eb319ac9624e8ec3440b00fea71
s133p-y <mpulido0001@mymail.lausd.net>
2025-06-02 09:30:30 -0700

    Update Example.java

--- src/main/java/com/km/Example.java
+++ src/main/java/com/km/Example.java
@@ -1,12 +1,18 @@
-package com.kr;
+package com.km;
 
 import com.birdbrain.Finch; 
 public class Example{
     public static void main(String[] args){
         Finch bird = new Finch();
-        setMotors(double leftSpeed, double rightSpeed){
              bird.setMotors(-50,50);
-        }
+            bird.setMove("F",10,50);
+            bird.setTurn("R",90,100);
+             bird.setBeak(0,100,0);
+             bird.print("hello");
+             bird.playNote(50,1);
+             bird.setMove("B",10,50);
+             bird.playNote(60,0.5);
+             
         // add code here
         bird.stop();
         bird.disconnect();
