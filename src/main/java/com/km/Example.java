cda581e59b5c1f7d79a552ebc1638e1634ef9293
s133p-y <mpulido0001@mymail.lausd.net>
2025-06-06 11:17:39 -0700

    Update Example.java

--- src/main/java/com/km/Example.java
+++ src/main/java/com/km/Example.java
@@ -7,12 +7,29 @@ public class Example{
              bird.setMotors(-50,50);
             bird.setMove("F",10,50);
             bird.setTurn("R",90,100);
-             bird.setBeak(0,100,0);
-             bird.print("hello");
-             bird.playNote(50,1);
+             bird.setBeak(11,11,192);
+             bird.playNote(55,2);
+             bird.print("DANCE!");
+              bird.setBeak(127,0,255);
+             bird.playNote(80,2);
+              bird.setTail(3,255,13,53);
              bird.setMove("B",10,50);
-             bird.playNote(60,0.5);
+              bird.setTail(1,0,255,255);
+             bird.playNote(47,2);
+             bird.setBeak(11,11,192);
+              bird.setMove("F",10,50);
+              bird.setTail(2,255,0,127);
+             bird.playNote(92,2);
+              bird.setBeak(127,0,255);
+             bird.playNote(60,2);
              bird.setTurn("R",90,100);
+              bird.setTail(1,0,255,128);
+             bird.playNote(67,2);
+             bird.setBeak(11,11,192);
+              bird.setTail(4,255,255,0);
+             bird.setTurn("L",90,100);
+             bird.playNote(73,2);
+              bird.setBeak(127,0,255);
              bird.setTurn("R",90,100);
              
         // add code here
